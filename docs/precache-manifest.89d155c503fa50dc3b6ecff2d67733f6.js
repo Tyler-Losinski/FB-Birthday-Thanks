30f28e767895956df5692f2d86105dcd431657d8
Unknown <tyler.a.losinski@ndsu.edu>
2018-12-07 13:41:10 -0600

    Another attempt

---
+++ docs/precache-manifest.89d155c503fa50dc3b6ecff2d67733f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6284ce992036e514591f",
+    "url": "/FB-Birthday-Thanks/static/js/runtime~main.6284ce99.js"
+  },
+  {
+    "revision": "fc0ada56fd1ee79ce85d",
+    "url": "/FB-Birthday-Thanks/static/js/main.fc0ada56.chunk.js"
+  },
+  {
+    "revision": "781fda0f23b437ae61e2",
+    "url": "/FB-Birthday-Thanks/static/js/1.781fda0f.chunk.js"
+  },
+  {
+    "revision": "fc0ada56fd1ee79ce85d",
+    "url": "/FB-Birthday-Thanks/static/css/main.e726bf4b.chunk.css"
+  },
+  {
+    "revision": "745dcb1b4d17a07688ef80c7e324d60a",
+    "url": "/FB-Birthday-Thanks/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4edc1abd62e6946d0159ec8c4037c765.js"
+  "/FB-Birthday-Thanks/precache-manifest.89d155c503fa50dc3b6ecff2d67733f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/FB-Birthday-Thanks/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
